a557960dfc54c01535ef09f5ce80ef7970a94b0e
Clement Chan <clementchan@Clement-MacBook-Pro.local>
2020-04-15 22:54:41 +0800

    DSSANDBOX

---
+++ D3SANDBOX/index.js
@@ -0,0 +1,13 @@
+
+d3.select('h1').style('color','blue')
+.attr('id','heading')
+.text('Updated hello');
+
+
+d3.select("body").style("background-color", "grey");
+
+d3.select("body")
+  .selectAll("p")
+  .data([4, 8, 15, 16, 23, 42])
+  .enter().append("p")
+    .text(function(d) { return "I’m number " + d + "!"; });
